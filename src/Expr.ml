1c9860fb1eaced5997606da154080c274a8d39ed
Nikita Biryukov <nbiryukov96@gmail.com>
2019-02-21 01:04:27 +0300

    hw1

--- src/Expr.ml
+++ src/Expr.ml
@@ -35,13 +35,14 @@ let update x v s = fun y -> if x = y then v else s y
 (* An example of a non-trivial state: *)                                                   
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
-(* Some testing; comment this definition out when submitting the solution. *)
+(* Some testing; comment this definition out when submitting the solution.
 let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
     ) ["x"; "a"; "y"; "z"; "t"; "b"]
+*)
 
 (* Expression evaluator
 
@@ -50,5 +51,28 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
+let to_int b = if b then 1 else 0
+let to_bool i = i != 0
+
+let operator op =
+  match op with
+  | "+" -> ( + )
+  | "-" -> ( - )
+  | "*" -> ( * )
+  | "/" -> ( / )
+  | "%" -> ( mod )
+  | "<" -> to_int (left < right)
+  | ">" -> to_int (left > right)
+  | "<=" -> to_int (left <= right)
+  | ">=" -> to_int (left >= right)
+  | "==" -> to_int (left == right)
+  | "!=" -> to_int (left != right)
+  | "&&" -> to_int ((to_bool left) && (to_bool right))
+  | "!!" -> to_int ((to_bool left) || (to_bool right))
+
+
+let rec eval s expr = match expr with
+					| Const const -> const
+					| Var var -> s var
+					| Binop (op, left, right) -> (operator op) (eval s left) (eval s right);;
                     
