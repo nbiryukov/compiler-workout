00e1fa235904ce48761fc66bcda553c6de693882
Nikita Biryukov <nbiryukov96@gmail.com>
2019-03-07 19:05:05 +0300

    hw3

--- src/Language.ml
+++ src/Language.ml
@@ -44,7 +44,29 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let bool_of_int intValue = intValue != 0
+
+    let int_of_bool bolValue = if bolValue then 1 else 0
+
+    let eval_operator operator left right = match operator with
+      | "!!" -> int_of_bool (bool_of_int left || bool_of_int right)
+      | "&&" -> int_of_bool (bool_of_int left && bool_of_int right)
+      | "==" -> int_of_bool (left = right)
+      | "!=" -> int_of_bool (left != right)
+      | "<=" -> int_of_bool (left <= right)
+      | "<"  -> int_of_bool (left < right)
+      | ">=" -> int_of_bool (left >= right)
+      | ">"  -> int_of_bool (left > right)
+      | "+"  -> left + right
+      | "-"  -> left - right
+      | "*"  -> left * right
+      | "/"  -> left / right
+      | "%"  -> left mod right
+
+    let rec eval state expr = match expr with
+      | Const v -> v
+      | Var x -> state x
+      | Binop (operator, left, right) -> eval_operator operator (eval state left) (eval state right)
 
     (* Expression parser. You can use the following terminals:
 
@@ -52,8 +74,27 @@ module Expr =
          DECIMAL --- a decimal constant [0-9]+ as a string
    
     *)
+
+    let parse_binop operator = ostap(- $(operator)), (fun x y -> Binop (operator, x, y))
     ostap (
-      parse: empty {failwith "Not implemented yet"}
+      expr:
+        !(Ostap.Util.expr
+          (fun x -> x)
+          (Array.map (fun (a, operator) -> a, List.map parse_binop operator)
+            [|
+              `Lefta, ["!!"];
+              `Lefta, ["&&"];
+              `Nona , ["=="; "!="; "<="; "<"; ">="; ">"];
+              `Lefta, ["+"; "-"];
+              `Lefta, ["*"; "/"; "%"];
+            |]
+          )
+          primary
+          );
+      primary:
+          c: DECIMAL {Const c}
+        | x: IDENT {Var x}
+        | -"(" expr -")"
     )
 
   end
@@ -70,7 +111,7 @@ module Stmt =
     (* composition                      *) | Seq    of t * t with show
 
     (* The type of configuration: a state, an input stream, an output stream *)
-    type config = Expr.state * int list * int list 
+    type config = Expr.state * int list * int list
 
     (* Statement evaluator
 
@@ -78,11 +119,24 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval conf operator =
+      let (st, input, output) = conf in
+      match operator with
+        | Read var	-> (match input with
+                | x::rest -> (Expr.update var x st), rest, output
+                | [] -> failwith("input error")
+        )
+        | Write exp -> st, input, (output @ [Expr.eval st exp])
+        | Assign (var, exp) -> (Expr.update var (Expr.eval st exp) st), input, output
+        | Seq (f, s) -> eval (eval conf f) s
 
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not implemented yet"}
+      stmt:
+			x:IDENT ":=" e:!(Expr.expr) {Assign(x, e)}
+			  | "read" "(" x:IDENT ")" {Read x}
+			  | "write" "(" e:!(Expr.expr) ")" {Write e};
+  		parse: s:stmt ";" rest:parse {Seq(s, rest)} | stmt
     )
       
   end
@@ -90,7 +144,7 @@ module Stmt =
 (* The top-level definitions *)
 
 (* The top-level syntax category is statement *)
-type t = Stmt.t    
+type t = Stmt.t
 
 (* Top-level evaluator
 
--- src/SM.ml
+++ src/SM.ml
@@ -24,7 +24,26 @@ type config = int list * Stmt.config
 
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-let eval _ = failwith "Not yet implemented"
+ let eval_inter conf prog =
+	let (stack, f) = conf in
+	let (st, input, output) = f in
+	match prog with
+    | BINOP operator -> (match stack with
+                          | y::x::rest -> [Language.Expr.eval_operator operator x y] @ rest, f
+                        )
+    | CONST x -> [x] @ stack, f
+    | READ -> (match input with
+    		              | x::rest -> [x] @ stack, (st, rest, output)
+              )
+    | WRITE -> (match stack with
+    		              | x::rest -> rest, (st, input, output @ [x])
+               )
+	| LD var -> [st var] @ stack, f
+	| ST var -> (match stack with
+		              | x::rest -> rest, (Language.Expr.update var x st, input, output)
+                )
+
+let eval conf program = List.fold_left eval_inter conf program
 
 (* Top-level evaluation
 
@@ -41,4 +60,13 @@ let run p i = let (_, (_, _, o)) = eval ([], (Language.Expr.empty, i, [])) p in 
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-let compile _ = failwith "Not yet implemented"
+let rec eval_expr expr = match expr with
+ | Language.Expr.Const v -> [CONST v]
+ | Language.Expr.Var x -> [LD x]
+ | Language.Expr.Binop (operator, left, right) -> (eval_expr left) @ (eval_expr right) @ [BINOP operator]
+
+let rec compile program = match program with
+ | Language.Stmt.Read cur -> [READ; ST cur]
+ | Language.Stmt.Write expr -> (eval_expr expr) @ [WRITE]
+ | Language.Stmt.Assign (cur, expr) -> (eval_expr expr) @ [ST cur]
+ | Language.Stmt.Seq (f, s) -> (compile f) @ (compile s)
